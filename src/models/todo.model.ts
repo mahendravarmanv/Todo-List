5f9865b049590ea226157269b1ee72199891a8e6
Mahendra Varma <mahendra.varma@jungleworks.com>
2021-11-13 16:59:23 +0530

    Files added

---
+++ src/models/todo.model.ts
@@ -0,0 +1,9 @@
+import { FormGroupDirective } from "@angular/forms";
+import {Guid} from 'guid-typescript';
+export class Todo{
+    constructor(
+        public id: Guid,
+        public title:String,
+        public isComplete:boolean
+    ){ }
+}
